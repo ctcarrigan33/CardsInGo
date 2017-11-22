8b613220bb3fe131791378f70216a70bab1548fe
Charlie Carrigan <ctcarrigan33@gmail.com>
2017-11-21 22:44:21 -0500

    missed line

--- deck.go
+++ deck.go
@@ -7,9 +7,14 @@ import "fmt"
 
 type deck []string
 
-// loop through deck of cards, print values
+// receiver loops through deck of cards, prints values
+// now, any var of type 'deck' has access to 'print'
+// 'd' is reference to actual instance of the deck we're using
+// here, 'd' points to result of cards.print() in main.go
+// similar to 'this' in JavaScript
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
 	}
+
 }
